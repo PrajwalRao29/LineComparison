40eb3cef641d8f0239d844a64f2f1cc2367f405d
PrajwalRao29 <raoprajrao@gmail.com>
2020-09-25 10:14:09 +0530

    UC1

--- LineComparison2.java
+++ LineComparison2.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 
 public class LineComparison2 {
 	public static void main (String args[])
 	{
 		System.out.println("Welcome to Line Computation Comparison program");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int d=sc.nextInt();
+		int x=(a-c)*(a-c);
+		int y=(b-d)*(b-d);
+		double z=Math.sqrt(x+y);
+		System.out.println("The length of the Line is "+z);
 	}
 }
