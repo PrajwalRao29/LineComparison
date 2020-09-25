ff77988d820831ecfafaa42370656e0c6cf2c25c
PrajwalRao29 <raoprajrao@gmail.com>
2020-09-25 10:15:47 +0530

    UC2

--- LineComparison2.java
+++ LineComparison2.java
@@ -1,9 +1,8 @@
+  
 import java.util.Scanner;
-
 public class LineComparison2 {
-	public static void main (String args[])
+	double Length()
 	{
-		System.out.println("Welcome to Line Computation Comparison program");
 		Scanner sc=new Scanner(System.in);
 		int a=sc.nextInt();
 		int b=sc.nextInt();
@@ -12,6 +11,28 @@ public class LineComparison2 {
 		int x=(a-c)*(a-c);
 		int y=(b-d)*(b-d);
 		double z=Math.sqrt(x+y);
-		System.out.println("The length of the Line is "+z);
+		return z;
+	}
+	void equal()
+	{	System.out.println("Enter the coordinates of first line");
+		String p=String.valueOf(Length());
+		System.out.println("Enter the coordinates of second line");
+		String q=String.valueOf(Length());
+		if(p.equals(q))
+		{
+			System.out.println("THE TWO LINES ARE EQUAL");
+		}
+		else
+		{
+			System.out.println("THE TWO LINES ARE NOT EQUAL");
+		}
+	}
+	
+	public static void main (String a0rgs[])
+	{
+		LineComparison2 l=new LineComparison2();
+		Scanner sc=new Scanner(System.in);
+		l.equal();	
 	}
 }
+		
